29615900e979ddaac37468eab8143d22f9e5c193
ezesanege <151142912+EzequielSanchezEgea@users.noreply.github.com>
2025-04-30 14:11:03 +0200

    Update SecurityConfig.java passwordencoder

--- src/main/java/com/ezequiel/reiunio/config/SecurityConfig.java
+++ src/main/java/com/ezequiel/reiunio/config/SecurityConfig.java
@@ -32,4 +32,8 @@ public class SecurityConfig {
         return http.build();
     
 }
+     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
