6732cc1b7ef076be9115d6a61b847e48b425d5c6
ezesanege <151142912+EzequielSanchezEgea@users.noreply.github.com>
2025-04-30 14:17:32 +0200

    Update SecurityConfig.java

--- src/main/java/com/ezequiel/reiunio/config/SecurityConfig.java
+++ src/main/java/com/ezequiel/reiunio/config/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -30,9 +33,9 @@ public class SecurityConfig {
             );
             
         return http.build();
+    }
     
-}
-     @Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
